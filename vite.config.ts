5eac0dfef862f10ac5bb1af93caf88cbd9b9ddb6
Daniel Spofford <868014+danielspofford@users.noreply.github.com>
2025-10-07 14:49:28 -0600

    tan the stack

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,64 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import { wrapVinxiConfigWithSentry } from "@sentry/tanstackstart-react";
+
+const config = defineConfig({
+  plugins: [
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tailwindcss(),
+    tanstackStart({
+      prerender: {
+        // Enable prerendering
+        enabled: true,
+
+        // Enable if you need pages to be at `/page/index.html` instead of `/page.html`
+        autoSubfolderIndex: true,
+
+        // How many prerender jobs to run at once
+        concurrency: 14,
+
+        // Whether to extract links from the HTML and prerender them also
+        crawlLinks: true,
+
+        // Filter function takes the page object and returns whether it should prerender
+        filter: ({ path }) => !path.startsWith("/do-not-render-me"),
+
+        // Number of times to retry a failed prerender job
+        retryCount: 2,
+
+        // Delay between retries in milliseconds
+        retryDelay: 1000,
+
+        // Callback when page is successfully rendered
+        onSuccess: ({ page }) => {
+          console.log(`Rendered ${page.path}!`);
+        },
+      },
+      // Optional configuration for specific pages (without this it will still automatically
+      // prerender all routes)
+      // pages: [
+      //   {
+      //     path: "/my-page",
+      //     prerender: { enabled: true, outputPath: "/my-page/index.html" },
+      //   },
+      // ],
+    }),
+    viteReact(),
+  ],
+});
+
+export default wrapVinxiConfigWithSentry(config, {
+  org: process.env.VITE_SENTRY_ORG,
+  project: process.env.VITE_SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  // Only print logs for uploading source maps in CI
+  // Set to `true` to suppress logs
+  silent: !process.env.CI,
+});
